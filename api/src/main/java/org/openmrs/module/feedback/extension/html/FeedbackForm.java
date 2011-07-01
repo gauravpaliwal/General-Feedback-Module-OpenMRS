f1b8beb72f9405de3159de1b9529c95a50413de1
gauravpaliwal <gauravpaliwal@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2011-07-01 13:15:18 +0000

    Except correct user permissions , and two other changes suggested by burke everything is almost complete :) atleast what minimal is required.Also right now testing it with extension point with getURL will soon replace it with the Portlets.It works fine if you have headerfull.jsp as this one : http://pastebin.com/YMAa2z0a
    git-svn-id: http://svn.openmrs.org/openmrs-modules/feedback/trunk@21205 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/module/feedback/extension/html/FeedbackForm.java
+++ api/src/main/java/org/openmrs/module/feedback/extension/html/FeedbackForm.java
@@ -1,20 +1,21 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
+
 package org.openmrs.module.feedback.extension.html;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import org.openmrs.module.Extension;
-import org.openmrs.module.web.extension.AdministrationSectionExt;
-import org.openmrs.module.web.extension.LinkExt;
 import org.openmrs.module.web.extension.PortletExt;
-/**
- *
- * @author gaurav
- */
+
 public class FeedbackForm extends PortletExt {
 
     public String getLabel() {
@@ -30,7 +31,7 @@ public class FeedbackForm extends PortletExt {
     }
 
     public String getPortletUrl() {
-        return "newjsp";
+        return "submitFeedbackController";
     }
 
     public String getPortletParameters() {
--- api/src/main/java/org/openmrs/module/feedback/extension/html/HeaderInclude.java
+++ api/src/main/java/org/openmrs/module/feedback/extension/html/HeaderInclude.java
@@ -1,13 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
+
 package org.openmrs.module.feedback.extension.html;
 
-/**
- *
- * @author gaurav
- */
+
 import java.util.List;
 import java.util.ArrayList;
 import org.openmrs.module.web.extension.HeaderIncludeExt;

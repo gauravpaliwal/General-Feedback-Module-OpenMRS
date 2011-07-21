b8c9c4edb4b07a348db67ff4903c967d20bccf57
gauravpaliwal <gauravpaliwal@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2011-07-21 11:46:54 +0000

    Getting various error , commiting for other
    git-svn-id: http://svn.openmrs.org/openmrs-modules/feedback/trunk@21774 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- omod/src/test/java/org/openmrs/module/feedback/web/AddFeedbackFormControllerTest.java
+++ omod/src/test/java/org/openmrs/module/feedback/web/AddFeedbackFormControllerTest.java
@@ -1,62 +1,81 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
+
 package org.openmrs.module.feedback.web;
+
+import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.openmrs.test.BaseContextSensitiveTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.feedback.FeedbackService ;
+
 
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.openmrs.web.controller.person.PersonFormController;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-/**
- *
- * @author gaurav
- */
-public class AddFeedbackFormControllerTest {
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class AddFeedbackFormControllerTest extends BaseModuleWebContextSensitiveTest {
 	
-	public AddFeedbackFormControllerTest() {
-	}
 
+	
+	
 	@BeforeClass
-	public static void setUpClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownClass() throws Exception {
-	}
-	
-	@Before
 	public void setUp() {
+		// code that will be invoked before this test starts
 	}
 	
-	@After
-	public void tearDown() {
+	@Test
+	public void aTest() {
+		System.out.println("Test");
 	}
-
-	/**
-	 * Test of formBackingObject method, of class AddFeedbackFormController.
-	 */
+	
 	@Test
 	public void testFormBackingObject() throws Exception {
 		System.out.println("formBackingObject");
-		HttpServletRequest request = null;
-		AddFeedbackFormController instance = new AddFeedbackFormController();
-		Boolean expResult = null;
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse(); ;
+		ModelAndView mv = new ModelAndView();
+		HttpServletRequest req = null;
+		/*AddFeedbackFormController instance = new AddFeedbackFormController();	*/
+		AddFeedbackFormController instance = new AddFeedbackFormController() ;
+		instance.setSuccessView("someValue") ;
+		request.setSession(new MockHttpSession(null));
+		request.setMethod("POST") ; 
+		request.addParameter("predefinedSubject", "Testing");
+		mv = instance.handleRequest(request, response) ;
+		
 		Boolean result = instance.formBackingObject(request);
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
+		System.out.println(instance.getFormView()) ;
+		System.out.println(response.getContentAsString()) ;
+		System.out.println(result) ;
+		/*Boolean expResult = null;
+		assertEquals(expResult, result);*/
+
 	}
 
-	/**
-	 * Test of referenceData method, of class AddFeedbackFormController.
-	 */
-	@Test
+/*	@Test
 	public void testReferenceData() throws Exception {
 		System.out.println("referenceData");
 		HttpServletRequest req = null;
@@ -65,6 +84,10 @@ public class AddFeedbackFormControllerTest {
 		Map result = instance.referenceData(req);
 		assertEquals(expResult, result);
 		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
+	}
+*/	
+	@AfterClass
+	public void cleanUp() {
+		// code that will be invoked after this test ends
 	}
 }
--- omod/src/test/java/org/openmrs/module/feedback/web/AddPredefinedSubjectFormControllerTest.java
+++ omod/src/test/java/org/openmrs/module/feedback/web/AddPredefinedSubjectFormControllerTest.java
@@ -1,77 +1,88 @@
-/**
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- *
- * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
-
 package org.openmrs.module.feedback.web;
+
+
+
+import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.feedback.FeedbackService ;
+
 
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.Assert.assertEquals;
 
 
-public class AddPredefinedSubjectFormControllerTest {
-    
-    public AddPredefinedSubjectFormControllerTest() {
-    }
+public class AddPredefinedSubjectFormControllerTest  extends BaseModuleWebContextSensitiveTest {
+	
+	@BeforeClass
+	public void setUp() throws Exception {
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
+		
+		// code that will be invoked before this test starts
+	}
+	
+	@Test
+	public void testFormBackingObject() throws Exception {
+		System.out.println("holla");
+		HttpServletRequest req = null;
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse(); 
+		AddPredefinedSubjectFormController instance = new AddPredefinedSubjectFormController();
+		initializeInMemoryDatabase();
+		authenticate() ;
+		Boolean expResult = null;
+		Boolean result = instance.formBackingObject(request);
+		System.out.println("formBackingObject");
+		ModelAndView mv = new ModelAndView();
+		instance.setSuccessView("someValue") ;
+		request.setSession(new MockHttpSession(null));
+		request.setMethod("POST") ; 
+		request.addParameter("predefinedSubject", "Testing");
+		mv = instance.handleRequest(request, response) ;
+		FeedbackService hService = (FeedbackService)Context.getService(FeedbackService.class);
+		System.out.println(hService.getPredefinedSubjects()) ;
+		System.out.println(instance.getFormView()) ;
+		System.out.println(response.getContentAsString()) ;
+		System.out.println(result) ;
+		/*Boolean expResult = null;
+		assertEquals(expResult, result);*/
+	}
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
+	
+	@AfterClass
+	public void cleanUp() {
+		// code that will be invoked after this test ends
+	}
 
-    /**
-     * Test of formBackingObject method, of class AddPredefinedSubjectFormController.
-     */
-    @Test
-    public void testFormBackingObject() throws Exception {
-        System.out.println("formBackingObject");
-        HttpServletRequest request = null;
-        AddPredefinedSubjectFormController instance = new AddPredefinedSubjectFormController();
-        Boolean expResult = null;
-        Boolean result = instance.formBackingObject(request);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of referenceData method, of class AddPredefinedSubjectFormController.
-     */
-    @Test
-    public void testReferenceData() throws Exception {
-        System.out.println("referenceData");
-        HttpServletRequest req = null;
-        AddPredefinedSubjectFormController instance = new AddPredefinedSubjectFormController();
-        Map expResult = null;
-        Map result = instance.referenceData(req);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+	/**
+	 * Test of formBackingObject method, of class AddPredefinedSubjectFormController.
+	 */
+	
+	/**
+	 * Test of referenceData method, of class AddPredefinedSubjectFormController.
+	 */
+	@org.junit.Test
+	public void testReferenceData() throws Exception {
+		System.out.println("referenceData");
+		HttpServletRequest req = null;
+		AddPredefinedSubjectFormController instance = new AddPredefinedSubjectFormController();
+		Map expResult = null;
+		Map result = instance.referenceData(req);
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+	}
 }
